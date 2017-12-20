d8b102ef0072e5376f31dacfddd14c921db1bcc4
hilpitome <hilpitome@gmail.com>
2017-12-20 20:02:17 +0300

    one more test and its done

--- app/src/main/java/com/warivirtualpos/wari/TransferRequestDetailActivity.java
+++ app/src/main/java/com/warivirtualpos/wari/TransferRequestDetailActivity.java
@@ -3,10 +3,12 @@ package com.warivirtualpos.wari;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +17,15 @@ import android.widget.TextView;
 import com.warivirtualpos.wari.model.Agent;
 import com.warivirtualpos.wari.model.TransferRequestData;
 import com.warivirtualpos.wari.utils.DatabaseHandler;
+import com.warivirtualpos.wari.utils.WariSecrets;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class TransferRequestDetailActivity extends AppCompatActivity implements View.OnClickListener {
     private  DatabaseHandler databaseHandler;
@@ -27,6 +38,8 @@ public class TransferRequestDetailActivity extends AppCompatActivity implements 
     private TransferRequestData transferRequestData;
 
     private int sqliteId;
+
+    private String mUrl = WariSecrets.mUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,14 +94,21 @@ public class TransferRequestDetailActivity extends AppCompatActivity implements 
         String confirmString = confirmationEt.getText().toString().trim();
         switch (id){
             case R.id.ok_btn:
-
+                SmsManager smsManager = SmsManager.getDefault();
                 if(confirmString.length()<1){
-                    confirmString = "";
+                    startActivity(new Intent(TransferRequestDetailActivity.this, MainActivity.class));
                 }
                 databaseHandler.updateTransferRequestConfirmation(sqliteId, confirmString);
                 Agent agent  = databaseHandler.checkIfAgent(transferRequestData.getAgentNumber());
                 int balance = transferRequestData.getAmount() + Integer.valueOf(agent.getSdBalance());
+                // update agent online balance
+                UpdateOnlineDatabaseTask updateOnlineDatabase = new UpdateOnlineDatabaseTask();
+                updateOnlineDatabase.execute(agent);
+                // update local sqlite database
                 databaseHandler.updateSqliteBalance(balance, transferRequestData.getAgentNumber());
+
+                String smsMessage = "You have been authorized to accept "+ transferRequestData.getAmount()+"from "+transferRequestData.getSenderLastName();
+                smsManager.sendTextMessage(transferRequestData.getAgentNumber(), null, smsMessage, null, null);
                 startActivity(new Intent(TransferRequestDetailActivity.this, MainActivity.class));
                 break;
             case  R.id.cancel_btn:
@@ -101,5 +121,44 @@ public class TransferRequestDetailActivity extends AppCompatActivity implements 
                 break;
         }
 
+    }
+    private class UpdateOnlineDatabaseTask extends AsyncTask<Agent, String, String> {
+
+    @Override
+        protected String doInBackground(Agent... params) {
+            OkHttpClient client = new OkHttpClient();
+            RequestBody formBody = null;
+
+            String resp = "";
+            Agent agent = params[0];
+
+            formBody = new FormBody.Builder()
+                    .add("sd_number", agent.getSdNumber())
+                    .add("last_balance", agent.getSdBalance())
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(mUrl)
+                    .post(formBody)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                resp = response.body().string();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+            return resp;
+
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            System.out.println(s);
+
+
+        }
     }
 }
